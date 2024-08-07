97863d962a649a03a86460a2f59c1620f5449294
lololozhkin <alexand3r.lozhkin@gmail.com>
2024-08-07 20:26:04 +0500

    asdf

--- export.go
+++ export.go
@@ -5,14 +5,18 @@ import (
 	"github.com/HazyCorp/govnilo/pkg/hazycheck"
 )
 
-type Checker = hazycheck.Checker
+type (
+	Checker   = hazycheck.Checker
+	CheckerID = hazycheck.CheckerID
+	Sploit    = hazycheck.Sploit
+	SploitID  = hazycheck.SploitID
 
-type Sploit = hazycheck.Sploit
+	Provider = hazycheck.Provider
+)
 
-type Provider = hazycheck.Provider
+var (
+	Execute = cmd.Execute
 
-var Execute = cmd.Execute
-
-var RegisterChecker = hazycheck.RegisterChecker
-
-var RegisterSploit = hazycheck.RegisterSploit
+	RegisterChecker = hazycheck.RegisterChecker
+	RegisterSploit  = hazycheck.RegisterSploit
+)
