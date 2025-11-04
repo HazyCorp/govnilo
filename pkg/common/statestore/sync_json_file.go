be94b4e6c3ed040112527b2bcf6ba6cd595c7c95
lololozhkin <alexand3r.lozhkin@gmail.com>
2025-11-04 11:10:30 +0300

    fix atomic rename in file storage

--- pkg/common/statestore/sync_json_file.go
+++ pkg/common/statestore/sync_json_file.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"log/slog"
 	"os"
+	"path"
 	"sync"
 
 	"github.com/HazyCorp/govnilo/pkg/common/hzlog"
@@ -86,7 +87,8 @@ func (j *SyncJsonFile[TState]) Flush(ctx context.Context) error {
 
 // writeStateToTemp returns name of the tempfile
 func (j *SyncJsonFile[TState]) writeStateToTemp(st *TState) (string, error) {
-	tmp, err := os.CreateTemp("/tmp/", "json_file_storage")
+	tmpDir := path.Dir(j.c.Path)
+	tmp, err := os.CreateTemp(tmpDir, "__json_file_storage")
 	if err != nil {
 		return "", errors.Wrap(err, "cannot open temporary file to make changes atomically")
 	}
