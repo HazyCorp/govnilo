23fd74d7d282794634b0eccefeee76d544846bd8
lololozhkin <alexand3r.lozhkin@gmail.com>
2025-11-06 11:45:36 +0300

    validating settings to avoid 0 denominator

--- internal/checkerctrl/settings_provider.go
+++ internal/checkerctrl/settings_provider.go
@@ -26,11 +26,22 @@ func validateSettings(
 			if checkerDesc == nil {
 				return errors.Errorf("checker description cannot be nil, but checker %s:%s was", svcName, checkerName)
 			}
+
+			if checkerDesc.Check.RunOptions.Rate.Per.AsDuration() == 0 {
+				return errors.Errorf("%s.checkers.%s.check.runOptions.rate.per cannot be 0", svcName, checkerName)
+			}
+			if checkerDesc.Get.RunOptions.Rate.Per.AsDuration() == 0 {
+				return errors.Errorf("%s.checkers.%s.get.runOptions.rate.per cannot be 0", svcName, checkerName)
+			}
 		}
 
 		for sploitName, sploitDesc := range svc.Sploits {
 			if sploitDesc == nil {
 				return errors.Errorf("checker description cannot be nil, but checker %s:%s was", svcName, sploitName)
+			}
+
+			if sploitDesc.RunOptions.Rate.Per.AsDuration() == 0 {
+				return errors.Errorf("%s.sploits.%s.runOptions.rate.per cannot be 0", svcName, sploitName)
 			}
 		}
 	}
